782f78a6b9b8aeee77f9dcee6be7dff44c6a144d
Griffon2012 <trifonov-ga@yandex.ru>
2022-10-13 19:36:14 +0300

    add if

---
+++ if/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string userName = Console.ReadLine();
+
+if(userName.ToLower() == "маша")
+{
+    Console.Write("Ура, это же ");
+}
+else
+{
+    Console.Write("Привет, ");
+ 
+}
+Console.WriteLine(userName);
